8129afcf16981e520c2fa3cc0640732b1ab6021b
Oopsno <yufeiliu94@gmail.com>
2016-07-12 10:30:22 +0800

    Exercise 1-7

--- hw02/HW02.hs
+++ hw02/HW02.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW02 where
 
+import Data.List
+
 -- Mastermind -----------------------------------------
 
 -- A peg can be one of six colors
@@ -23,43 +25,61 @@ colors = [Red, Green, Blue, Yellow, Orange, Purple]
 
 -- Get the number of exact matches between the actual code and the guess
 exactMatches :: Code -> Code -> Int
-exactMatches = undefined
+exactMatches = curry $ foldl (flip ((+).(.?.))) 0 . uncurry (zipWith (==))
+  where (.?.) True  = 1
+        (.?.) False = 0
 
 -- Exercise 2 -----------------------------------------
 
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors xs = map (length . ($ xs) . elemIndices) colors
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches = curry $ sum . uncurry (flip ((zipWith min) . countColors) . countColors)
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess =
+  let em = exactMatches secret guess
+      mm = matches      secret guess
+      nm = mm - em
+  in  Move guess em nm
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move code em nm) other = 
+  let em' = exactMatches code other
+      mm' = matches      code other 
+      nm' = mm' - em'
+  in em == em' && nm == nm'
+  
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move = filter (isConsistent move)
 
 -- Exercise 6 -----------------------------------------
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes 1         = map (:[]) colors
+allCodes n | n > 1 = concatMap (\x -> map (x:) (allCodes (n - 1))) colors
+allCodes _         = []
 
 -- Exercise 7 -----------------------------------------
 
 solve :: Code -> [Move]
-solve = undefined
+solve secret = solve' . allCodes $ length secret
+  where solve' [] = []
+        solve' (x:xs) =
+          let move = getMove secret x
+              rest = filterCodes move xs
+          in move:(solve' rest)
 
 -- Bonus ----------------------------------------------
 
