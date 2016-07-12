001d7eb6a45a2ba483a7f3989f95f47ec49d54d9
Oopsno <yufeiliu94@gmail.com>
2016-07-12 17:42:18 +0800

    Exercise 1-9

--- hw06/HW06.hs
+++ hw06/HW06.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
-module HW06 where
+module HW06 ( main ) where
 
 import Data.List
 import Data.Functor
@@ -7,15 +7,17 @@ import Data.Functor
 -- Exercise 1 -----------------------------------------
 
 fib :: Integer -> Integer
-fib = undefined
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [0..]
 
 -- Exercise 2 -----------------------------------------
 
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 1:scanl (+) 1 fibs2
 
 -- Exercise 3 -----------------------------------------
 
@@ -27,56 +29,65 @@ instance Show a => Show (Stream a) where
              ++ ",..."
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons curr rest) = curr : streamToList rest
 
 -- Exercise 4 -----------------------------------------
 
 instance Functor Stream where
-    fmap = undefined
+    fmap fn (Cons curr rest) = Cons (fn curr) (fmap fn rest)
 
 -- Exercise 5 -----------------------------------------
 
 sRepeat :: a -> Stream a
-sRepeat = undefined
+sRepeat x = Cons x (sRepeat x)
 
 sIterate :: (a -> a) -> a -> Stream a
-sIterate = undefined
+sIterate f x = let fx = f x in Cons x (sIterate f fx)
 
 sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (Cons _ _) _ = undefined
+sInterleave (Cons curr rest) second = Cons curr (sInterleave second rest)
 
 sTake :: Int -> Stream a -> [a]
-sTake = undefined
+sTake 0 _ = []
+sTake n (Cons curr rest) = curr : sTake (n - 1) rest
 
 -- Exercise 6 -----------------------------------------
 
 nats :: Stream Integer
-nats = undefined
+nats = sIterate (+1) 0
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = fmap (ru . (+1)) nats
+  where ru n = if even n then 1 + ru (div n 2) else 0
 
 -- Exercise 7 -----------------------------------------
 
 -- | Implementation of C rand
 rand :: Int -> Stream Int
-rand = undefined
+rand = sIterate prand
+  where prand r = (1103515245 * r + 12345) `mod` 2147483648
 
 -- Exercise 8 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: ~110 MB -}
 minMaxSlow :: [Int] -> Maybe (Int, Int)
 minMaxSlow [] = Nothing   -- no min or max if there are no elements
 minMaxSlow xs = Just (minimum xs, maximum xs)
 
 -- Exercise 9 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: ~0.04 MB -}
 minMax :: [Int] -> Maybe (Int, Int)
-minMax = undefined
+minMax [] = Nothing
+minMax (x:xs) = kernel x x xs
+  where kernel min max [] = Just (min, max)
+        kernel min max (x:xs) =
+          let min' = if min > x then x else min
+              max' = if max < x then x else max
+          in  min' `seq` max' `seq` kernel min' max' xs
 
 main :: IO ()
-main = print $ minMaxSlow $ sTake 1000000 $ rand 7666532
+main = print $ minMax $ sTake 1000000 $ rand 7666532
 
 -- Exercise 10 ----------------------------------------
 
