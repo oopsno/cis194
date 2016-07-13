f98635c76ed47b130a7436fadbae48803ba22d7d
Oopsno <yufeiliu94@gmail.com>
2016-07-13 10:34:56 +0800

    HW05 - Done

--- hw05/HW05.hs
+++ hw05/HW05.hs
@@ -2,7 +2,10 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module HW05 where
 
+import Control.Monad
 import Data.ByteString.Lazy (ByteString)
+import Data.Bits
+import Data.List
 import Data.Map.Strict (Map)
 import System.Environment (getArgs)
 
@@ -14,42 +17,56 @@ import Parser
 -- Exercise 1 -----------------------------------------
 
 getSecret :: FilePath -> FilePath -> IO ByteString
-getSecret = undefined
+getSecret raw sec = BS.pack . filter (/=0) <$> liftM2 (BS.zipWith xor) (BS.readFile raw) (BS.readFile sec)
 
 -- Exercise 2 -----------------------------------------
 
 decryptWithKey :: ByteString -> FilePath -> IO ()
-decryptWithKey = undefined
+decryptWithKey key path = 
+ BS.pack . BS.zipWith xor (BS.cycle key) <$> BS.readFile (path ++ ".enc") >>= BS.writeFile path 
 
 -- Exercise 3 -----------------------------------------
 
 parseFile :: FromJSON a => FilePath -> IO (Maybe a)
-parseFile = undefined
+parseFile path = decode <$> BS.readFile path
 
 -- Exercise 4 -----------------------------------------
 
 getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
-getBadTs = undefined
+getBadTs v t = do
+  tids  <- parseFile v :: IO (Maybe [TId])
+  trans <- parseFile t :: IO (Maybe [Transaction])
+  return $ liftM2 check tids trans
+  where check :: [TId] -> [Transaction] -> [Transaction]
+        check xs = filter (\y -> tid y `notElem` xs)
 
 -- Exercise 5 -----------------------------------------
 
 getFlow :: [Transaction] -> Map String Integer
-getFlow = undefined
+getFlow ts = foldl up (Map.fromList (zip (map from ts ++ map to ts) (repeat 0))) ts
+  where up m t = let n  = amount t
+                     m' = Map.adjust (+ n) (to t) m
+                 in  Map.adjust (flip (-) n) (from t) m'
 
 -- Exercise 6 -----------------------------------------
 
 getCriminal :: Map String Integer -> String
-getCriminal = undefined
+getCriminal = fst . head . sortOn snd . Map.toList 
 
 -- Exercise 7 -----------------------------------------
 
 undoTs :: Map String Integer -> [TId] -> [Transaction]
-undoTs = undefined
-
+undoTs m ts = 
+  let payers = descSort $ Map.toList $ Map.filter (> 0) m
+      payees = descSort $ Map.toList $ Map.filter (< 0) m
+  in zipWith3 payback payers payees ts
+  where descSort = reverse . sortOn (abs . snd)
+        payback (x, xa) (y, ya) = Transaction x y (minimum [xa, abs ya])
+  
 -- Exercise 8 -----------------------------------------
 
 writeJSON :: ToJSON a => FilePath -> a -> IO ()
-writeJSON = undefined
+writeJSON p = BS.writeFile p . encode
 
 -- Exercise 9 -----------------------------------------
 
