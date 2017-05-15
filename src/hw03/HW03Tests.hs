f2eeb079e449417be2643b6c62d111c863c01077
YufeiLiu <yufeiliu94@gmail.com>
2017-05-15 18:15:40 +0800

    added tests

--- src/hw03/HW03Tests.hs
+++ src/hw03/HW03Tests.hs
@@ -1,6 +1,21 @@
-module HW03Tests ( tests ) where
+module Main where
 
-import Distribution.TestSuite
+import Prelude    ( IO, (>>), return )
 
-tests :: IO [Test]
-tests = return []
+import Test.HUnit
+import CIS194.HUnit
+import HW03
+
+tests :: Test
+tests =  test [ "exercise 1" ~: 0 ~=? (empty "A")
+              , "exercise 2" ~: test [ 5 ~=? evalE empty (Val 5)
+                                     , 0 ~=? evalE empty (Op (Val 1) Eql (Val 2))
+                                     ]
+              , "exercise 3" ~: desugar (Incr "A") ~=? DAssign "A" (Op (Var "A") Plus (Val 1))
+              , "exercise 4" ~: test [ 10 ~=? evalSimple empty (DAssign "A" (Val 10)) "A"
+                                     , 24 ~=?  run (extend empty "In" 4) factorial "Out"
+                                     ]
+              ]
+
+main :: IO ()
+main = runner tests
