e37cc19628185f19970174dc3b5ce67c69563c35
YufeiLiu <yufeiliu94@gmail.com>
2017-05-15 19:19:09 +0800

    updated hw 05 with single exe

--- src/hw05/Main.hs
+++ src/hw05/Main.hs
@@ -1,21 +1,22 @@
 module Main ( main ) where
 
 import Parser
-import HW05
-
 import System.Environment   ( getArgs )
+
+import qualified HW05
+import qualified Paths_cis194 as P
+
+doEverything :: [FilePath] -> IO String
+doEverything (a:b:c:x:y:z:_) = HW05.doEverything a b c x y z
 
 main :: IO ()
 main = do
   args <- getArgs
-  crim <- 
-    case args of
-      dog1:dog2:trans:vict:ids:out:_ ->
-          doEverything dog1 dog2 trans vict ids out
-      _ -> doEverything "dog-original.jpg"
-                        "dog.jpg"
-                        "transactions.json"
-                        "victims.json"
-                        "new-ids.json"
-                        "new-transactions.json"
-  putStrLn crim
+  defaultPathes <- mapM P.getDataFileName [ "dog-original.jpg"
+                                          , "dog.jpg"
+                                          , "transactions.json"
+                                          , "victims.json"
+                                          , "new-ids.json"
+                                          , "new-transactions.json" ]
+  let files = if length args >= 6 then args else defaultPathes
+  doEverything files >>= putStrLn
